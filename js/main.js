07afe1c1700b77eb06ad2dc9eebd1d4c4091d29e
rickstewart <rick.stewart@ns.sympatico.ca>
2016-11-07 15:21:41 -0400

    new game loop

--- js/main.js
+++ js/main.js
@@ -9,18 +9,25 @@
     var AudioContext;
     var audioCtx;
     var oscillator;
-    var redSegment;
-    var greenSegment;
-    var yellowSegment;
-    var blueSegment;
     var pattern;
     var loopCounter;
+    var playedToneCount;
+    var computerLoopRun;
+    var postComputerLoopCleanup;
+    var playerLoopRun;
+    var computersTurn;
+    var playersMoves;
+    var clickEnabled;
+    var maskClickEvents;
+    var setTimeoutOne;
+    var trackLevel;
+    var redSegment = $('#game-top-left');                 // create references to HTML screen areas.
+    var greenSegment = $('#game-top-right');
+    var yellowSegment = $('#game-bottom-left');
+    var blueSegment = $('#game-bottom-right');
+    var fccLink = $('#fcc-link');
 
     function init() {
-        redSegment = $('#game-top-left');                 // create references to HTML screen areas.
-        greenSegment = $('#game-top-right');
-        yellowSegment = $('#game-bottom-left');
-        blueSegment = $('#game-bottom-right');
         colors = ['9c121c', '03A64B', 'CBA60C', '094A8F'];
         if (!window.AudioContext) {
             alert('you browser does not support Web Audio API');
@@ -28,25 +35,75 @@
         AudioContext = window.AudioContext || window.webkitAudioContext;
         audioCtx = new AudioContext();
         FLASH_INTERVAL = 200;
+        playedToneCount = 0;
+        playersMoves = [];
+        computerLoopRun = true;
+        postComputerLoopCleanup = true;
+        playerLoopRun = false;
+        clickEnabled = false;
+        maskClickEvents = false;
         setTimeout(function() {
-            go();
+            gameLoop();                     // delay game start so that display finishes initialization.
         }, 600);
     }
 
-    function go() {
+    function gameLoop() {
         pattern = simonPatternGenerator();
-        loopCounter = pattern.length;
-        timingLoop();
+        loopCounter = trackLevel = 3;
+        setInterval(function() {
+            if(computerLoopRun) {
+                clickEnabled = false;
+                computerLoopRun = false;
+                computersTurn = true;
+                computerTurnTimingLoop();
+            }
+            if (postComputerLoopCleanup && playedToneCount === trackLevel + 1) {
+                postComputerLoopCleanup = false;
+                computersTurn = false;
+                setTimeout(function() {
+                    clickEnabled = true;
+                }, 400);
+                playerLoopRun = true;
+                playedToneCount = [];
+                clearTimeout(setTimeoutOne);
+                console.log('timingLoop done...');
+            }
+            if(playedToneCount === trackLevel + 1 && playerLoopRun) {
+                console.log('player made third move...');
+                playerLoopRun = false;
+                clickEnabled = false;
+                setTimeout(function() {
+                    testPlayerResponse();
+                }, 100);
+            }
+        },1000);
     }
 
-    function timingLoop() {
-        setTimeout(function() {
-            simonPlays(pattern[loopCounter]);
-            loopCounter--;
-            if(loopCounter >= 0) {
-                timingLoop();
+    function testPlayerResponse() {
+        if(playersMoves.length >= trackLevel) {
+            console.log('testPlayerResponse() firing player made moves');
+            for(var i = 0; i <= trackLevel; i++) {
+                if(playersMoves[i] !== pattern[i]) {
+                    playersMoves = [];
+                    loopCounter = trackLevel;
+                    setTimeout(function() {
+                        errorFlash();
+                    }, 600);
+                    return;
+                }
             }
-        },600);
+            trackLevel++;
+            loopCounter = trackLevel;
+            playersMoves = [];
+        }
+    }
+
+    function computerTurnTimingLoop() {
+        setTimeoutOne = setTimeout(function() {
+            chooseColorAndPlay(pattern[loopCounter], 'computer');
+            loopCounter--;
+            computerTurnTimingLoop();
+        },900);
     }
 
     function playTone(freq, toneLength) {
@@ -55,10 +112,100 @@
         oscillator.connect(audioCtx.destination);
         oscillator.frequency.value = freq;
         oscillator.start();
+        playedToneCount++;
         setTimeout(function() {
             oscillator.disconnect(audioCtx.destination);
             oscillator = null;
         }, toneLength);
+    }
+
+    function chooseColorAndPlay(color, whoClicked) {
+        if(whoClicked === 'computer') {
+            console.log('chooseColorAndPlay() computers turn firing');
+            switch (color) {
+                case 0:
+                    flashRed();
+                    break;
+                case 1:
+                    flashGreen();
+                    break;
+                case 2:
+                    flashYellow();
+                    break;
+                case 3:
+                    flashBlue();
+                    break;
+            }
+        }
+        else if(whoClicked === 'player' && clickEnabled && maskClickEvents) {
+            console.log('chooseColorAndPlay() players turn firing');
+            switch (color) {
+                case 0:
+                    flashRed();
+                    playersMoves.unshift(0);
+                    break;
+                case 1:
+                    flashGreen();
+                    playersMoves.unshift(1);
+                    break;
+                case 2:
+                    flashYellow();
+                    playersMoves.unshift(2);
+                    break;
+                case 3:
+                    flashBlue();
+                    playersMoves.unshift(3);
+                    break;
+            }
+        }
+    }
+
+    function errorFlash() {
+        redSegment.css('background-color', '#ff1c2b');
+        greenSegment.css('background-color', '#00ff6e');
+        yellowSegment.css('background-color', '#f6ff00');
+        blueSegment.css('background-color', '#1188ff');
+        playTone(560, FLASH_INTERVAL);
+        setTimeout(function () {
+            redSegment.css('background-color', '#9c121c');
+            greenSegment.css('background-color', '#03A64B');
+            yellowSegment.css('background-color', '#CBA60C');
+            blueSegment.css('background-color', '#094A8F');
+        }, FLASH_INTERVAL);
+    }
+
+    function flashRed() {
+        if(!maskClickEvents) {
+            redSegment.css('background-color', '#ff1c2b');
+            playTone(164, FLASH_INTERVAL);
+            setTimeout(function () {
+                redSegment.css('background-color', '#9c121c');
+            }, FLASH_INTERVAL);
+        }
+    }
+
+    function flashGreen() {
+        greenSegment.css('background-color', '#00ff6e');
+        playTone(220, FLASH_INTERVAL);
+        setTimeout(function () {
+            greenSegment.css('background-color', '#03A64B');
+        }, FLASH_INTERVAL);
+    }
+
+    function flashYellow() {
+        yellowSegment.css('background-color', '#f6ff00');
+        playTone(261, FLASH_INTERVAL);
+        setTimeout(function () {
+            yellowSegment.css('background-color', '#CBA60C');
+        }, FLASH_INTERVAL);
+    }
+
+    function flashBlue() {
+        blueSegment.css('background-color', '#1188ff');
+        playTone(329, FLASH_INTERVAL);
+        setTimeout(function () {
+            blueSegment.css('background-color', '#094A8F');
+        }, FLASH_INTERVAL);
     }
 
     function simonPatternGenerator() {
@@ -68,83 +215,39 @@
             randomColor = Math.floor(Math.random() * 4);   // random number 0 - 3 inclusive.
             twentyRandomColors.push(randomColor);
         }
-        return twentyRandomColors;
+        //return twentyRandomColors;
+        return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
     }
 
-    function flashRed() {
-        redSegment.css('background-color', '#ff1c2b');
-        playTone(164, 200);
-        setTimeout(function () {
-            redSegment.css('background-color', '#9c121c');
-        }, FLASH_INTERVAL);
-    }
-
-    function flashGreen() {
-        greenSegment.css('background-color', '#00ff6e');
-        playTone(220, 200);
-        setTimeout(function () {
-            greenSegment.css('background-color', '#03A64B');
-        }, FLASH_INTERVAL);
-    }
-
-    function flashYellow() {
-        yellowSegment.css('background-color', '#f6ff00');
-        playTone(261, 200);
-        setTimeout(function () {
-            yellowSegment.css('background-color', '#CBA60C');
-        }, FLASH_INTERVAL);
-    }
-
-    function flashBlue() {
-        blueSegment.css('background-color', '#1188ff');
-        playTone(329, 200);
-        setTimeout(function () {
-            blueSegment.css('background-color', '#094A8F');
-        }, FLASH_INTERVAL);
-    }
-
-    function simonPlays(color) {
-        switch (color) {
-            case 0:
-                flashRed();
-                break;
-            case 1:
-                flashGreen();
-                break;
-            case 2:
-                flashYellow();
-                break;
-            case 3:
-                flashBlue();
-                break;
-        }
-    }
-
-    $('#game-top-left').click(function() {
-        simonPlays(0);
+    redSegment.click(function() {
+        chooseColorAndPlay(0, 'player');
+        maskClickEvents = true;
+        setTimeout(function() {
+            maskClickEvents = false;
+        }, 400);
     });
 
-    $('#game-top-right').click(function() {
-        simonPlays(1);
+    greenSegment.click(function() {
+        chooseColorAndPlay(1, 'player');
     });
 
-    $('#game-bottom-left').click(function() {
-        simonPlays(2);
+    yellowSegment.click(function() {
+        chooseColorAndPlay(2, 'player');
     });
 
-    $('#game-bottom-right').click(function() {
-        simonPlays(3);
+    blueSegment.click(function() {
+        chooseColorAndPlay(3, 'player');
     });
 
-    $('#fcc-link').mouseover(function () {
+    fccLink.mouseover(function () {
         this.style.color = 'yellow';
     });
 
-    $('#fcc-link').mouseout(function () {
+    fccLink.mouseout(function () {
         this.style.color = '#795151';
     });
 
-    $('#fcc-link').click(function () {
+    fccLink.click(function () {
         window.open('https://www.freecodecamp.com/challenges/build-a-simon-game');
     });
 
